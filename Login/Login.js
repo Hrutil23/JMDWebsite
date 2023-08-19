1abd61fb79beeb6d057d7274d1d188ed42861d5d
ppatel2301 <84679497+ppatel2301@users.noreply.github.com>
2023-08-19 16:51:13 -0500

    Delete Login directory

--- Login/Login.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loginButton = document.getElementById('login-button');
-
-    loginButton.addEventListener('click', function() {
-        // Navigate to the home.html page
-        window.location.href = '/Home.html';
-    });
-});
