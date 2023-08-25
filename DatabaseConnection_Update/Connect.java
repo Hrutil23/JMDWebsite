76a4f8ae3ad4b388b3c75d779e37b539b2e6e90e
ppatel2301 <dharmspatel2003@gmail.com>
2023-08-25 12:55:42 -0500

    Database_Connection

---
+++ DatabaseConnection_Update/Connect.java
@@ -0,0 +1,40 @@
+package DatabaseConnection_Update;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+ */
+
+ public class Connect{
+
+    private static String username = "Username";
+    private static String passsword = "Pasword";
+    private static String databaseID = "DatabaseID";
+    private static String connUrl = "jdbc:oracle:thin:" + username + "/" + passsword + "@dbHost:1521:" + databaseID;
+
+    public static void main(String[] args) {
+
+        connectToDatabase(connUrl);
+    }
+
+    private static void connectToDatabase(String url)
+    {
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            if (conn != null) {
+                System.out.println("Connected");
+            }
+            
+        }catch(SQLException e)
+        {
+            System.out.println("Database did not establish a connection.");
+        }
+    }
+}
---
+++ DatabaseConnection_Update/Update.java
@@ -0,0 +1,12 @@
+package DatabaseConnection_Update;
+
+public class Update extends Connect{
+
+    // Method to update the calander events for particular student
+    // This wil update the calander on the home screen
+
+    // Method to update the grades for particular student(Marksheet)
+
+
+    // Method to update the anoucement that a student sees
+}
