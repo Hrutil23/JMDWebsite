b8529448f53b6263b1e87f09d4ef54316d2aedd5
ppatel2301 <dharmspatel2003@gmail.com>
2023-09-05 22:18:23 -0500

    Login Page Updated. Func still not working...

--- Login.js
+++ Login.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
     const loginButton = document.getElementById('login-button');
 
     loginButton.addEventListener('click', function() {
         // Navigate to the home.html page
-        window.location.href = 'Home.html';
+
+        if(username == "Test" && password == "Test")
+        {
+            window.location.href = 'Home.html';
+        }else{
+            console.log("NOT VALID USER NAME:)");
+        }
     });
 });
