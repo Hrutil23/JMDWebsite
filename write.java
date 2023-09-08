0fe0f3e2b2a3a27acb222ac2eae5d87856a0cf75
Hrutil <108419472+Hrutil23@users.noreply.github.com>
2023-09-08 18:57:35 -0500

    Create write.java
    Writing initial code for the class which will handel the text base database

---
+++ write.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.*; 
+
+public class write {
+
+    public static void main(String[] args) {
+
+        // Nothing much here
+
+        String text = "Welcome to geekforgeeks\nHappy Learning!";
+
+        // Defining the file name of the file
+        Path fileName = Path.of("/Users/hrutilpatel/Desktop/Jmd webiste /JMD/Write/text.txt"); 
+
+        try{ 
+
+            // Writing into the file
+            Files.writeString(fileName, text);
+
+        }catch (Exception e)
+        { 
+
+                System.out.println("Not worked"); 
+
+        }
+
+    }
+
+}
