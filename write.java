ef05bef286857b770fe9253f64de7b154a0ff3a5
Hrutil <108419472+Hrutil23@users.noreply.github.com>
2023-09-08 19:19:41 -0500

    Update write.java

--- write.java
+++ write.java
@@ -9,22 +9,25 @@ public class write {
 
     public static void main(String[] args) {
 
-        // Nothing much here
-
-        String text = "Welcome to JMD Math class ";
-
         // Defining the file name of the file
-        Path fileName = Path.of("/Users/hrutilpatel/Desktop/Jmd webiste /JMD/Write/text.txt"); 
+        Path fileName = Path.of("test.txt"); 
+
+        write("Welcome to JMD Math class ", fileName); 
+
+    }
+
+    public static void write (String data, Path fileName )
+    { 
 
         try{ 
 
             // Writing into the file
-            Files.writeString(fileName, text);
+            Files.writeString(fileName, data);
 
         }catch (Exception e)
         { 
 
-                System.out.println("Not worked"); 
+            System.out.println("Encounter some error"); 
 
         }
 
